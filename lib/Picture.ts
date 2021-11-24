a850be764b2cc5a055f184e219a397cca328aedd
TohsakaKuro <TohsakaKuro@qq.com>
2021-11-24 22:11:19 +0800

    doc: update

--- lib/Picture.ts
+++ lib/Picture.ts
@@ -1,15 +1,24 @@
 import fs, { Dirent } from 'fs';
 import path from 'path';
 type pick = {
+    // 图片的名称
     name: string,
+    // 图片的服务器路径
     path: string,
 }
+// 定义一个图片结构数组,作为缓存
 let picks: pick[];
+// 定义刷新数组的方法
 function frush() {
+    // 扫描一个文件夹
     const dirs: Dirent[] = fs.readdirSync('./public/random-pick', { withFileTypes: true });
+    // 清空缓存
     picks = [];
+    // 循环这个文件夹下的文件
     for (const dir of dirs) {
+        // 如果是文件
         if (dir.isFile()) {
+            // 把这个文件放入pic数组
             picks.push({
                 name: dir.name,
                 path: path.resolve(__dirname, dir.name),
@@ -17,10 +26,15 @@ function frush() {
         }
     }
 }
+// 执行一次刷新缓存
 frush();
+// 设置循环,每5000秒执行一次刷新缓存方法
 setInterval(frush, 5000);
+// 定义获取一个图片方法
 function getOne(): pick | null {
     const pickLength = picks.length;
+    // 这里有几率数组越界导致返回undefind, 作业: 修复它
     return picks[Math.floor(Math.random() * (pickLength + 1))];
 }
+// 把getOne方法导出,供其他地方调用
 export { getOne }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,32 @@
 import React from 'react';
+// 把getOne方法从其他模块导入
 import { getOne } from '../lib/Picture';
 
 class Index extends React.Component<{
   pickDir: string,
 }> {
   render() {
+    // react  jsx(tsx)语法, 返回一个div元素 <div></div> 包裹的内容作为子元素,整体返回
     return <div style={{ position:'fixed', display: 'flex', width: '100%', height: '100%', overflow: 'auto' }}>
-      {this.props.pickDir ? <img style={{ margin: 'auto' }} src={this.props.pickDir} /> : <div>连个图片都没有看毛线</div>}
+      { // {大括号, react 语法,表示接下来的内容是表达式
+        // 三元运算符, js语法, 如果 this.props.pickDir是有值的, 返回一个img元素, 否则返回一个div元素
+        // <img 标签, src属性设置图片地址, 这里 src=this.props.pickDir. 我们已经在下面导出一个方法,在服务器渲染时已经获得了返回值.这里可以直接使用.
+        this.props.pickDir ? <img style={{ margin: 'auto' }} src={this.props.pickDir} /> : <div>连个图片都没有看毛线</div>
+      }
     </div>
   }
 }
-export function getServerSideProps() {
+// nextjs 框架约定方法, 该方法将在服务器执行,返回props用于服务器渲染页面
+export function getServerSideProps(): { props: { pickDir: string }} {
+  // 获取一个随机图片
   const pic = getOne();
-  let pickDir = null;
+  let pickDir: string = null;
   if (pic) {
+    // 如果图片有值的话, 返回图片的http相对地址
     pickDir = `/random-pick/${pic.name}`;
   }
+  // 返回对象
   return { props: { pickDir } }
 }
+// 导出这个React类,供React识别
 export default Index;
