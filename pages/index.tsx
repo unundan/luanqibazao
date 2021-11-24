dfb0fefb1b3fe9d3d6fb544cb02a0f28d5be4a2d
TohsakaKuro <TohsakaKuro@qq.com>
2021-11-24 21:57:21 +0800

    fix: pick scorll

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,14 @@ class Index extends React.Component<{
   pickDir: string,
 }> {
   render() {
-    return <div style={{ position: 'fixed', display: 'flex', width: '100%', height: '100%' }}>
+    return <div style={{ position:'fixed', display: 'flex', width: '100%', height: '100%', overflow: 'auto' }}>
       {this.props.pickDir ? <img style={{ margin: 'auto' }} src={this.props.pickDir} /> : <div>连个图片都没有看毛线</div>}
     </div>
   }
 }
 export function getServerSideProps() {
   const pic = getOne();
-  let pickDir;
+  let pickDir = null;
   if (pic) {
     pickDir = `/random-pick/${pic.name}`;
   }
